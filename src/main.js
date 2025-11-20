736ef1ab00e613d060a97e91d0807717f8ed42c8
jhonatan <137637410+Jhotis12@users.noreply.github.com>
2025-11-20 14:40:07 -0500

    Cambio de imagenes

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-
+import img1 from "./icon/img1.png"
+import img2 from "./icon/img2.jpg"
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="No-Apto/src/icon/Imagen-3-generated-image-sunset-painting-monet.original.png" target="_blank">
-      <img src="public/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="No-Apto/src/icon/photo-1689308271305-58e75832289b.jpg" target="_blank">
-      <img src="src/javascript.svg" class="logo vanilla" alt="JavaScript logo" />
-    </a>
+    
+      <img src="${img1}"  alt="img 1" />
+      <img src="${img2}"  alt="img 2" style ="height:450px" />
+      
+    
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
